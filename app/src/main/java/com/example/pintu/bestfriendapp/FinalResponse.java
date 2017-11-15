abd14a779b1c9f60b123a0dabbebe1c7da1e54a1
purbashaa <soni.kashish@gmail.com>
2017-11-15 15:28:31 +0100

    7th commit..Creation of FinalResponse Activity

---
+++ app/src/main/java/com/example/pintu/bestfriendapp/FinalResponse.java
@@ -0,0 +1,13 @@
+package com.example.pintu.bestfriendapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinalResponse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_response);
+    }
+}
