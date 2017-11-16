8b44fe2ebb54c6349166ba1e85ce4aaba2cbb3bd
purbashaa <soni.kashish@gmail.com>
2017-11-16 01:03:55 +0100

    8th commit..Addition of images

--- app/src/main/java/com/example/pintu/bestfriendapp/UserInteraction.java
+++ app/src/main/java/com/example/pintu/bestfriendapp/UserInteraction.java
@@ -17,10 +17,123 @@ public class UserInteraction extends AppCompatActivity {
 
 
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(UserInteraction.this,
+        Spinner mySpinner1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> myAdapter1 = new ArrayAdapter<String>(UserInteraction.this,
                 android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
+        myAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner1.setAdapter(myAdapter1);
+
+
+        Spinner mySpinner2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> myAdapter2 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner2.setAdapter(myAdapter2);
+
+
+        Spinner mySpinner3 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<String> myAdapter3 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner3.setAdapter(myAdapter3);
+
+
+
+        Spinner mySpinner4 = (Spinner) findViewById(R.id.spinner4);
+        ArrayAdapter<String> myAdapter4 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner4.setAdapter(myAdapter4);
+
+
+        Spinner mySpinner5 = (Spinner) findViewById(R.id.spinner5);
+        ArrayAdapter<String> myAdapter5 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner5.setAdapter(myAdapter5);
+
+
+
+        Spinner mySpinner6 = (Spinner) findViewById(R.id.spinner6);
+        ArrayAdapter<String> myAdapter6 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner6.setAdapter(myAdapter6);
+
+
+
+        Spinner mySpinner7 = (Spinner) findViewById(R.id.spinner7);
+        ArrayAdapter<String> myAdapter7 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner7.setAdapter(myAdapter7);
+
+
+
+        Spinner mySpinner8 = (Spinner) findViewById(R.id.spinner8);
+        ArrayAdapter<String> myAdapter8 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter8.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner8.setAdapter(myAdapter8);
+
+
+
+        Spinner mySpinner9 = (Spinner) findViewById(R.id.spinner9);
+        ArrayAdapter<String> myAdapter9 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter9.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner9.setAdapter(myAdapter9);
+
+
+
+        Spinner mySpinner10 = (Spinner) findViewById(R.id.spinner10);
+        ArrayAdapter<String> myAdapter10 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter10.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner10.setAdapter(myAdapter10);
+
+
+
+        Spinner mySpinner11 = (Spinner) findViewById(R.id.spinner11);
+        ArrayAdapter<String> myAdapter11 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter11.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner11.setAdapter(myAdapter11);
+
+
+
+        Spinner mySpinner12 = (Spinner) findViewById(R.id.spinner12);
+        ArrayAdapter<String> myAdapter12 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter12.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner12.setAdapter(myAdapter12);
+
+
+
+        Spinner mySpinner13 = (Spinner) findViewById(R.id.spinner13);
+        ArrayAdapter<String> myAdapter13 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter13.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner13.setAdapter(myAdapter13);
+
+
+
+        Spinner mySpinner14 = (Spinner) findViewById(R.id.spinner14);
+        ArrayAdapter<String> myAdapter14 = new ArrayAdapter<String>(UserInteraction.this,
+                android.R.layout.simple_list_item_1, getResources() .getStringArray(R.array.numbers));
+        myAdapter14.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner14.setAdapter(myAdapter14);
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
